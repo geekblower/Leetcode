62e827bd85b114c30f9805628e6f493bb8bc5c70
Akash Gautam <76935019+geekblower@users.noreply.github.com>
2022-07-10 09:37:20 +0530

    Added solution - LeetHub

---
+++ Brain Game - GFG/brain-game.cpp
@@ -0,0 +1,44 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution{
+	public:
+    bool brainGame(vector<int>nums) {
+        vector<int> arr(1005);
+    
+        for(int i=2; i<=1000; i++) {
+            for(int j=2*i; j<=1000; j+=i)
+                arr[j] = max(arr[j], 1+arr[i]);
+        }
+        
+        int x = 0;
+        
+        for(int i=0; i<nums.size(); i++){
+            x = x ^ arr[nums[i]];
+        }
+        
+        if(!x)
+            return false;
+        return true;
+    }
+};
+
+// { Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n;
+		cin >> n;
+		vector<int>nums(n);
+		for(int i = 0; i < n; i++)cin >> nums[i];
+		Solution ob;
+		bool ans = ob.brainGame(nums);
+		if(ans)
+			cout << "A\n";
+		else cout << "B\n";
+	}  
+	return 0;
+}  // } Driver Code Ends

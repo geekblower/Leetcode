b793ca4694c23661755b25be3211685625804fb0
Akash Gautam <76935019+geekblower@users.noreply.github.com>
2022-07-14 07:35:53 +0530

    Time: 8 ms (94.86%), Space: 5.9 MB (31.51%) - LeetHub

---
+++ 9-palindrome-number/9-palindrome-number.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x < 0)
+            return false;
+        
+        long int rev = 0;
+        int temp = x;
+        
+        while(temp > 0) {
+            rev = (rev*10) + (temp%10);
+            temp/=10;
+        }
+        
+        if(rev == x)
+            return true;
+        
+        return false;
+    }
+};

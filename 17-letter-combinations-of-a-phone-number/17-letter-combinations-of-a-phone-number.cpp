b6726e5f6d8ec23f390aa612a3b752c0f9f5d2e9
Akash Gautam <76935019+geekblower@users.noreply.github.com>
2022-08-08 21:51:04 +0530

    Time: 2 ms (52.55%), Space: 6.4 MB (94.68%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
@@ -1,25 +1,32 @@
 class Solution {
 public:
-    vector<string> letterCombinations(string digits) {
-        vector<string> result;
-        
-        if(digits.size()==0)
-            return result;
-        
-        string charmap[10] = {"0","1","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
-        
-        result.push_back("");
-        
-        for(int i=0; i<digits.size(); i++) {
-            vector<string> temp;
-            string chars = charmap[digits[i] - '0'];
-            for(int c=0; c<chars.size(); c++) {
-                for(int j=0; j<result.size(); j++)
-                    temp.push_back(result[j] + chars[c]);
-            }
-            result=temp;
+    void solve(string digits, string output, int index, vector<string>& ans, string mapping[]) {
+        if(index >= digits.length()) {
+            ans.push_back (output);
+            return;
         }
         
-        return result;
+        int number = digits[index] - 48;
+        string value = mapping[number];
+        
+        for(int i=0; i<value.length(); i++) {
+            output.push_back (value[i]);
+            solve(digits, output, index+1, ans, mapping);
+            output.pop_back();
+        }
+    }
+    
+    vector<string> letterCombinations(string digits) {
+        vector<string> ans;
+        string output = "";
+        int index = 0;
+        string mapping[10] = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        
+        if(digits.length() <= 0)
+            return ans;
+        
+        solve(digits, output, index, ans, mapping);
+        
+        return ans;
     }
 };

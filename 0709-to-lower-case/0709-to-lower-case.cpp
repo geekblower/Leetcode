f9312edfc380ef228eb9cd8b0b789a8f94deb46b
Akash Gautam <76935019+geekblower@users.noreply.github.com>
2022-11-02 23:31:45 +0530

    Time: 4 ms (29.38%), Space: 6.1 MB (82.30%) - LeetHub

---
+++ 0709-to-lower-case/0709-to-lower-case.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string toLowerCase(string s) {
+        
+        for(int i=0; i<s.size(); i++) {
+            if(s[i]>=65 && s[i]<=90)
+                s[i] += 32;
+        }
+        
+        return s;
+    }
+};

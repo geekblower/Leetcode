53521022823440f0e1f5c9be0b7daeb5dfb480db
Akash Gautam <76935019+geekblower@users.noreply.github.com>
2023-02-27 21:46:07 +0530

    Added solution - LeetHub

---
+++ Java Hello World - GFG/java-hello-world.java
@@ -0,0 +1,22 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        Solution ob = new Solution();
+        ob.printHelloWorld();
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution{
+    static void printHelloWorld(){
+        System.out.println("Hello World");
+    }
+}

aa827bfc3e69b522e2a18b43175ea1adf59e3a8d
Akash Gautam <76935019+geekblower@users.noreply.github.com>
2023-02-27 22:12:28 +0530

    Added solution - LeetHub

---
+++ Sum Of Digits - GFG/sum-of-digits.cpp
@@ -0,0 +1,34 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+public:
+    int sumOfDigits(int N){
+        int sum = 0;
+        
+        while(N) {
+            sum += (N%10);
+            N /= 10;
+        }
+        
+        return sum;
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin>>N;
+        Solution ob;
+        cout << ob.sumOfDigits(N) << endl;
+    }
+    return 0; 
+}
+// } Driver Code Ends

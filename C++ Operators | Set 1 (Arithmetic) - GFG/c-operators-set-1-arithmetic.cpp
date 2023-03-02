df71301a5c1a5588c09d0c60ea30577a1f0f7656
Akash Gautam <76935019+geekblower@users.noreply.github.com>
2023-03-02 20:37:50 +0530

    Added solution - LeetHub

---
+++ C++ Operators | Set 1 (Arithmetic) - GFG/c-operators-set-1-arithmetic.cpp
@@ -0,0 +1,25 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    vector<int> cppOperators(int A, int B) {
+        return {A+B, A*B, (A>B)?(A-B):(B-A), (A>B)?(A/B):(B/A)};
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int A, B;
+        cin >> A >> B;
+        Solution ob;
+        vector<int> ans = ob.cppOperators(A, B);
+        for (int u : ans) cout << u << "\n";
+    }
+}
+// } Driver Code Ends

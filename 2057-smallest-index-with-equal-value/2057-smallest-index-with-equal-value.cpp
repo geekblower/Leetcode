eb599276dc7a84338594b1c9ed8fb31e551d91bf
Akash Gautam <76935019+geekblower@users.noreply.github.com>
2023-03-17 21:38:15 +0530

    Time: 11 ms (78.56%), Space: 22 MB (40.77%) - LeetHub

---
+++ 2057-smallest-index-with-equal-value/2057-smallest-index-with-equal-value.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int smallestEqual(vector<int>& nums) {
+        for(int i=0; i<nums.size(); i++) {
+            if(nums[i]%10 == i%10) {
+                return i;
+            }
+        }
+        
+        return -1;
+    }
+};

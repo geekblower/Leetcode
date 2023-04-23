8531b88e3d49133115547d635730ce0caa9b10ec
Akash Gautam <76935019+geekblower@users.noreply.github.com>
2023-04-24 00:06:23 +0530

    Time: 6 ms (21.95%), Space: 6.4 MB (28.26%) - LeetHub

--- 0202-happy-number/0202-happy-number.cpp
+++ 0202-happy-number/0202-happy-number.cpp
@@ -1,20 +1,27 @@
 class Solution {
 public:
-    long long power(int n) {
-        if(n<10)
-            return n*n;
-        return ((n%10) * (n%10)) + power(n/10);
-    }
-    
     bool isHappy(int n) {
+        map<int, bool> m;
+
         while(1) {
-            if(n==89)
-                return false;
+            int ans = 0;
 
-            if(n==1)
+            while(n != 0) {
+                int digit = n % 10;
+                ans += (digit * digit);
+                n = n / 10;
+            }
+
+            if(ans == 1)
                 return true;
-
-            n = power(n);
+            else if(m[ans] == true)
+                return false;
+            else {
+                m[ans] = true;
+                n = ans;
+            }
         }
+
+        return false;
     }
 };

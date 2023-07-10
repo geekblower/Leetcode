6cf867d84cad87a8a162089f5d321522c8be7443
Akash Gautam <76935019+geekblower@users.noreply.github.com>
2023-07-10 23:43:10 +0530

    Time: 15 ms (54.84%), Space: 22 MB (6.84%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -9,28 +9,28 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-
 class Solution {
 public:
-    void helper(TreeNode* root, vector<int> & ans) {
-        if(root==NULL)
+    void inOrder(TreeNode* root, vector<int>& ans) {
+        if(root == NULL) {
             return;
+        }
         
-        helper(root->left, ans);
+        inOrder(root->left, ans);
         ans.push_back (root->val);
-        helper(root->right, ans);
+        inOrder(root->right, ans);
     }
     
     bool isValidBST(TreeNode* root) {
         vector<int> ans;
-        helper(root, ans);
+        inOrder(root, ans);
         
         for(int i=0; i<ans.size()-1; i++) {
-            if(ans[i] >= ans[i+1])
+            if(ans[i] >= ans[i+1]) {
                 return false;
+            }
         }
         
         return true;
     }
-    
 };

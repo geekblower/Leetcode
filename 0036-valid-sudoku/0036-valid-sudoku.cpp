6a4d30aa54d0adba6fb9c9f1edd80256124061a8
Akash Gautam <76935019+geekblower@users.noreply.github.com>
2023-07-29 23:16:53 +0530

    Time: 25 ms (56.74%), Space: 21.6 MB (9.97%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,24 +1,48 @@
 class Solution {
 public:
-    bool isValidSudoku(vector<vector<char>>& board) {
-        vector<set<int>> rows(9), cols(9), blocks(9);
-        
+    bool validate(int row, int col, vector<vector<char>> board) {
+        char val = board[row][col];
+
         for(int i=0; i<9; i++) {
-            for(int j=0; j<9; j++) {
-                if(board[i][j] == '.')
-                    continue;
-                
-                int curr = board[i][j] - '0';
-                
-                if(rows[i].count(curr) || cols[j].count(curr) || blocks[(i/3)*3+j/3].count(curr)) 
+            // For row
+            if(board[row][i] == val) {
+                if(i != col)
                     return false;
-                
-                rows[i].insert(curr);
-                cols[j].insert(curr);
-                blocks[(i/3)*3+j/3].insert(curr);
+            }
+
+            // For column
+            if(board[i][col] == val) {
+                if(i != row)
+                    return false;
+            }
+
+            // For 3x3 matrix
+            if(board[3*(row/3) + i/3][3*(col/3) + i%3] == val) {
+                if(((3*(row/3) + i/3) != row) && ((3*(col/3) + i%3) != col))
+                    return false;
             }
         }
-        
+
         return true;
+    }
+
+    bool isValidSudoku(vector<vector<char>>& board) {
+
+        for(int row=0; row<9; row++) {
+            for(int col=0; col<9; col++) {
+                if(board[row][col] == '.') {
+                    continue;
+                }
+
+                bool temp = validate(row, col, board);
+
+                if(temp == false) {
+                    return temp;
+                }
+            }
+        }
+
+        return true;
+
     }
 };

ff155514a69daccafb4856bef6f1ff34f0a7fb43
Akash Gautam <76935019+geekblower@users.noreply.github.com>
2023-08-01 23:53:48 +0530

    Time: 3 ms (82.31%), Space: 20.5 MB (9.14%) - LeetHub

---
+++ 2798-number-of-employees-who-met-the-target/2798-number-of-employees-who-met-the-target.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int numberOfEmployeesWhoMetTarget(vector<int>& hours, int target) {
+        int count = 0;
+        
+        for(int i : hours) {
+            if(i >= target) {
+                count++;
+            }
+        }
+        
+        return count;
+    }
+};

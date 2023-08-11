e5eb602e794de0eafe62e349985aec8b2c8b5519
Akash Gautam <76935019+geekblower@users.noreply.github.com>
2023-08-11 23:53:29 +0530

    Time: 8 ms (38.02%), Space: 6.6 MB (41.28%) - LeetHub

--- 2810-faulty-keyboard/2810-faulty-keyboard.cpp
+++ 2810-faulty-keyboard/2810-faulty-keyboard.cpp
@@ -1,13 +1,6 @@
 class Solution {
 public:
-    string rev(string s) {
-        int start = 0;
-        int end = s.length() - 1;
-        return s;
-    }
-    
     string finalString(string s) {
-        
         string answer = "";
         
         for(int i=0; i<s.length(); i++) {
@@ -19,6 +12,5 @@ public:
         }
         
         return answer;
-        
     }
 };

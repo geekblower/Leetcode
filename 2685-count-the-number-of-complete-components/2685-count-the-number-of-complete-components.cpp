fe5f93a1dabea1bedc3c136082e9e959f501b9f0
Akash Gautam <76935019+geekblower@users.noreply.github.com>
2023-09-08 00:19:54 +0530

    Time: 210 ms (51.40%), Space: 135.4 MB (20.67%) - LeetHub

--- 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.cpp
+++ 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.cpp
@@ -1,33 +1,18 @@
 class Solution {
 public:
-    void dfsCount(unordered_map<int, list<int>> &adj, vector<bool> &visited, int i, pair<int,int> &ans) {
+    void dfs(unordered_map<int, list<int>> &adj, vector<bool> &visited, int i, pair<int,int> &ans) {
         visited[i] = true;
         ans.second++;
         
         for(int x : adj[i]) {
                 ans.first++;
+
             if(!visited[x]) {
-                // count++;
-                dfsCount(adj, visited, x, ans);
+                dfs(adj, visited, x, ans);
             }
         }
     }
 
-    bool dfs(unordered_map<int, list<int>> &adj, vector<bool> &visited, int i, int len) {
-        visited[i] = true;
-        
-        for(int x : adj[i]) {
-            if(adj[x].size() != len) return false;
-            
-            if(!visited[x]) {
-                bool ans = dfs(adj, visited, x, len);
-                if(ans == false) return false;
-            }
-        }
-        
-        return true;
-    }
-    
     int countCompleteComponents(int n, vector<vector<int>>& edges) {
         unordered_map<int, list<int>> adj;
         
@@ -44,13 +29,9 @@ public:
         
         for(int i=0; i<n; i++) {
             if(!visited[i]) {
-                int cnt = 0;
                 pair<int, int> ans({0,0});
-                // vector<bool> temp = visited;
-                dfsCount(adj, visited, i, ans);
-                // cout << "cnt : " << cnt << endl;
-                
-                // if(dfs(adj, visited, i, cnt+1)) count++;
+
+                dfs(adj, visited, i, ans);
                 
                 int nodeCount = ans.second;
                 int edgeCount = ans.first/2;

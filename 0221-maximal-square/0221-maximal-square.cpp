57a921fc53c4e6787ceec3ab00c4e925148e01b4
Akash Gautam <76935019+geekblower@users.noreply.github.com>
2023-09-10 11:58:45 +0530

    Time: 72 ms (59.61%), Space: 20.9 MB (44.31%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.cpp
+++ 0221-maximal-square/0221-maximal-square.cpp
@@ -40,6 +40,28 @@ public:
             return dp[i][j];
         }
     }
+    
+    int solveTab(vector<vector<char>>& matrix, int row, int col, int &maxi) {
+        vector<vector<int>> dp(row+1, vector<int>(col+1, 0));
+        
+
+        for(int i=row-1; i>=0; i--) {
+            for(int j=col-1; j>=0; j--) {
+                int right = dp[i][j+1];
+                int diagonal = dp[i+1][j+1];
+                int bottom = dp[i+1][j];
+
+                if(matrix[i][j] == '1') {
+                    dp[i][j] = 1 + min(right, min(bottom, diagonal));
+                    maxi = max(maxi, dp[i][j]);
+                } else {
+                    dp[i][j] = 0;
+                }        
+            }
+        }
+        
+        return maxi;
+    }
 
     int maximalSquare(vector<vector<char>>& matrix) {
         int row = matrix.size();
@@ -48,8 +70,10 @@ public:
         
         // solveRec(matrix, 0, 0, maxi);
         
-        vector<vector<int>> dp(row, vector<int>(col, -1));
-        solveMem(dp, matrix, 0, 0, maxi);
+        // vector<vector<int>> dp(row, vector<int>(col, -1));
+        // solveMem(dp, matrix, 0, 0, maxi);
+        
+        solveTab(matrix, row, col, maxi);
         return maxi*maxi;
     }
 };

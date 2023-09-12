5549532e7ae328ebf6633ac5de5da17087e8a947
Akash Gautam <76935019+geekblower@users.noreply.github.com>
2023-09-12 20:08:29 +0530

    Time: 1104 ms (9.43%), Space: 292.3 MB (9.20%) - LeetHub

---
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    int solveRec(vector<int> &nums, int curr, int prev) {
+        // Base case
+        if(curr == nums.size()) {
+            return 0;
+        }
+        
+        // Include
+        int incl = 0;
+        if(prev == -1 || nums[curr] > nums[prev]) {
+            incl = 1 + solveRec(nums, curr+1, curr);
+        }
+        
+        // Exclude
+        int excl = solveRec(nums, curr+1, prev);
+        
+        return max(incl, excl);
+    }
+    
+    int solveMem(vector<vector<int>> &dp, vector<int> &nums, int curr, int prev) {
+        // Base case
+        if(curr == nums.size()) {
+            return 0;
+        }
+        
+        if(dp[curr][prev+1] != -1) {
+            return dp[curr][prev+1];
+        }
+        
+        // Include
+        int incl = 0;
+        if(prev == -1 || nums[curr] > nums[prev]) {
+            incl = 1 + solveMem(dp, nums, curr+1, curr);
+        }
+        
+        // Exclude
+        int excl = solveMem(dp, nums, curr+1, prev);
+        
+        dp[curr][prev+1] = max(incl, excl);
+        return dp[curr][prev+1];
+    }
+    
+    int lengthOfLIS(vector<int>& nums) {
+        // return solveRec(nums, 0, -1);    
+        
+        vector<vector<int>> dp(nums.size(), vector<int>(nums.size()+1, -1));
+        return solveMem(dp, nums, 0, -1);
+    }
+};

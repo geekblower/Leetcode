9d5280d95b68eea9120602bdf7da3ef104807791
Akash Gautam <76935019+geekblower@users.noreply.github.com>
2023-09-12 22:06:00 +0530

    Time: 9 ms (86.45%), Space: 10.4 MB (71.87%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -86,6 +86,26 @@ public:
         return nextRow[0];
     }
     
+    int solveBinarySearch(vector<int> &nums) {
+        if(nums.size() == 0) {
+            return 0;
+        }
+        
+        vector<int> ans;
+        ans.push_back(nums[0]);
+        
+        for(int i=1; i<nums.size(); i++) {
+            if(nums[i] > ans.back()) {
+                ans.push_back(nums[i]);
+            } else {
+                int index = lower_bound(ans.begin(), ans.end(), nums[i]) - ans.begin();
+                ans[index] = nums[i];
+            }
+        }
+        
+        return ans.size();
+    }
+    
     int lengthOfLIS(vector<int>& nums) {
         // return solveRec(nums, 0, -1);    
         
@@ -94,6 +114,8 @@ public:
         
         // return solveTab(nums);
         
-        return solveSpc(nums);
+        // return solveSpc(nums);
+        
+        return solveBinarySearch(nums);
     }
 };

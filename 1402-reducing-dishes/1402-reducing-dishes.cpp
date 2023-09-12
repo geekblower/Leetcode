091f3726e0528bdba74cd5f0cdeee92282eaf308
Akash Gautam <76935019+geekblower@users.noreply.github.com>
2023-09-12 09:04:42 +0530

    Time: 17 ms (64.28%), Space: 8.4 MB (64.35%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.cpp
+++ 1402-reducing-dishes/1402-reducing-dishes.cpp
@@ -42,6 +42,24 @@ public:
         return dp[0][0];
     }
     
+    int solveSpc(vector<int> &sat, int n) {
+        vector<int> curr(n+1, 0);
+        vector<int> next(n+1, 0);
+        
+        for(int index=n-1; index>=0; index--) {
+            for(int time=index; time>=0; time--) {
+                int include = sat[index] * (time+1) + next[time+1];
+                int exclude = next[time];
+
+                curr[time] = max(include, exclude);
+            }
+            
+            next = curr;
+        }
+        
+        return next[0];
+    }
+    
     int maxSatisfaction(vector<int>& satisfaction) {
         // Sorting array
         sort(satisfaction.begin(), satisfaction.end());
@@ -52,6 +70,8 @@ public:
         // vector<vector<int>> dp(n, vector<int>(n+1, -1));
         // return solveMem(dp, satisfaction, 0, 0);
         
-        return solveTab(satisfaction, n);
+        // return solveTab(satisfaction, n);
+        
+        return solveSpc(satisfaction, n);
     }
 };

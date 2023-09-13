ea62e63ecdd9f8a34aec07cd4b723921ab118e1a
Akash Gautam <76935019+geekblower@users.noreply.github.com>
2023-09-13 09:19:47 +0530

    Time: 489 ms (15.18%), Space: 87.7 MB (26.21%) - LeetHub

---
+++ 0354-russian-doll-envelopes/0354-russian-doll-envelopes.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int solveBinarySearch(vector<vector<int>>& env, int n) {
+        if(n == 0) {
+            return 0;
+        }
+        
+        vector<int> ans;
+        ans.push_back(env[0][1]);
+        
+        for(int i=1; i<n; i++) {
+            if(env[i][1] > ans.back()) {
+                ans.push_back(env[i][1]);
+            } else {
+                int index = lower_bound(ans.begin(), ans.end(), env[i][1]) - ans.begin();
+                ans[index] = env[i][1];
+            }
+        }
+        
+        return ans.size();
+    }
+    
+    static bool compare(vector<int> &a, vector<int> &b) {
+        if(a[0] == b[0]) {
+            return a[1] > b[1];
+        } else {
+            return a[0] < b[0];
+        }
+    }
+    
+    int maxEnvelopes(vector<vector<int>>& envelopes) {
+        sort(envelopes.begin(), envelopes.end(), compare);
+        
+        for(auto x : envelopes) {
+            for(auto y : x) cout << y << " ";
+            cout << endl;
+        }
+        
+        return solveBinarySearch(envelopes, envelopes.size());
+    }
+};

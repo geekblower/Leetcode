6a03ece3363ba8eb6b4d589ee63bb8cc3927525b
Akash Gautam <76935019+geekblower@users.noreply.github.com>
2023-09-13 20:12:07 +0530

    Time: 18 ms (73.01%), Space: 23.6 MB (10.30%) - LeetHub

--- 1388-pizza-with-3n-slices/1388-pizza-with-3n-slices.cpp
+++ 1388-pizza-with-3n-slices/1388-pizza-with-3n-slices.cpp
@@ -27,6 +27,34 @@ public:
         return dp[index][n];
     }
     
+    int solveTab(vector<int> &slices, int k) {
+        vector<vector<int>> dp1(k+2, vector<int>(k+2, 0));
+        vector<vector<int>> dp2(k+2, vector<int>(k+2, 0));
+        
+        for(int index = k-2; index >= 0; index--) {
+            for(int n=1; n<=k/3; n++) {
+                int incl = slices[index] + dp1[index+2][n-1];
+                int excl = dp1[index+1][n];
+
+                dp1[index][n] = max(incl, excl);
+            }
+        }
+        
+        for(int index = k-1; index >= 1; index--) {
+            for(int n=1; n<=k/3; n++) {
+                int incl = slices[index] + dp2[index+2][n-1];
+                int excl = dp2[index+1][n];
+
+                dp2[index][n] = max(incl, excl);
+            }
+        }
+        
+        int opt1 = dp1[0][k/3];
+        int opt2 = dp2[1][k/3];
+        
+        return max(opt1, opt2);
+    }
+    
     int maxSizeSlices(vector<int>& slices) {
         int k = slices.size();
         int n = k/3;
@@ -34,12 +62,14 @@ public:
         // int opt1 = solveRec(slices, 0, k-2, n);
         // int opt2 = solveRec(slices, 1, k-1, n);
 
-        vector<vector<int>> dp1(k, vector<int>(k, -1));
-        int opt1 = solveMem(dp1, slices, 0, k-2, n);
+//         vector<vector<int>> dp1(k, vector<int>(k, -1));
+//         int opt1 = solveMem(dp1, slices, 0, k-2, n);
         
-        vector<vector<int>> dp2(k, vector<int>(k, -1));
-        int opt2 = solveMem(dp2, slices, 1, k-1, n);
+//         vector<vector<int>> dp2(k, vector<int>(k, -1));
+//         int opt2 = solveMem(dp2, slices, 1, k-1, n);
 
-        return max(opt1, opt2);        
+//         return max(opt1, opt2);        
+        
+        return solveTab(slices, k);
     }
 };

c0112475a4f54dbe727812279b97ce2557b4e038
Akash Gautam <76935019+geekblower@users.noreply.github.com>
2023-09-15 10:32:51 +0530

    Time: 2 ms (59.69%), Space: 6.4 MB (14.97%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -1,13 +1,36 @@
 class Solution {
 public:
-    int numTrees(int n) {
+    int solveRec(int n) {
         if(n <= 1) return 1;
         
         int ans = 0;
         for(int i=1; i<=n; i++) {
-            ans += numTrees(i-1) * numTrees(n-i);
+            ans += solveRec(i-1) * solveRec(n-i);
         }
         
         return ans;
+    }
+    
+    int solveMem(vector<int> &dp, int n) {
+        if(n <= 1) return 1;
+        
+        if(dp[n] != -1) {
+            return dp[n];
+        }
+        
+        int ans = 0;
+        for(int i=1; i<=n; i++) {
+            ans += solveMem(dp, i-1) * solveMem(dp, n-i);
+        }
+        
+        dp[n] = ans;
+        return dp[n];
+    }
+    
+    int numTrees(int n) {
+        // return solveRec(n);
+        
+        vector<int> dp(n+1, -1);
+        return solveMem(dp, n);
     }
 };

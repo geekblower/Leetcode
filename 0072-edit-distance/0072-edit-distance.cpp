25068fa3a63c465d066681c76751d51099a5f543
Akash Gautam <76935019+geekblower@users.noreply.github.com>
2023-09-20 09:44:34 +0530

    Time: 14 ms (43.84%), Space: 9.3 MB (23.01%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -53,10 +53,44 @@ public:
         return dp[i][j];
     }
     
+    int solveTab(string str1, string str2, int len1, int len2) {
+        vector<vector<int>> dp(len1 + 1, vector<int>(len2 + 1, 0));
+        
+        for(int j=0; j<len2; j++) {
+            dp[len1][j] = len2 - j;
+        }
+        
+        for(int i=0; i<len1; i++) {
+            dp[i][len2] = len1 - i;
+        }
+        
+        for(int i=len1-1; i>=0; i--) {
+            for(int j=len2-1; j>=0; j--) {
+                int ans = 0;
+        
+                if(str1[i] == str2[j]) {
+                    ans = dp[i+1][j+1];
+                } else {
+                    int insAns = 1 + dp[i][j+1];
+                    int delAns = 1 + dp[i+1][j];
+                    int repAns = 1 + dp[i+1][j+1];
+
+                    ans = min(insAns, min(delAns, repAns));
+                }
+
+                dp[i][j] = ans;
+            }
+        }
+        
+        return dp[0][0];
+    }
+    
     int minDistance(string word1, string word2) {
         // return solveRec(word1, word2, 0, 0);
         
-        vector<vector<int>> dp(word1.length(), vector<int>(word2.length(), -1));
-        return solveMem(dp, word1, word2, 0, 0);
+        // vector<vector<int>> dp(word1.length(), vector<int>(word2.length(), -1));
+        // return solveMem(dp, word1, word2, 0, 0);
+        
+        return solveTab(word1, word2, word1.length(), word2.length());
     }
 };

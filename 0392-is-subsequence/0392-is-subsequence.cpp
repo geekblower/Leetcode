3044524e4546605733fc1f7b85b7d1c3dd52d38a
Akash Gautam <76935019+geekblower@users.noreply.github.com>
2023-09-22 20:10:21 +0530

    Time: 0 ms (100.00%), Space: 8.7 MB (5.02%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -23,8 +23,38 @@ public:
         return false;
     }
     
+    bool solveMem(vector<vector<int>> &dp, string s, string t, int i, int j) {
+        if(i > s.length()) return true;
+        if(j > t.length()) return false;
+        if(dp[i][j] != -1) return dp[i][j];
+        
+        if(s[i] == t[j]) {
+            return solveMem(dp, s, t, i+1, j+1);
+        }
+        
+        bool ans = false;
+        while(j<t.length()) {
+            if(t[j] != s[i]) {
+                j++;
+            } else {
+                if(i==s.length()-1)
+                    ans = true;
+                else
+                    ans = solveMem(dp, s, t, i+1, j+1);
+                break;
+            }
+        }
+        
+        dp[i][j] = ans;
+        return dp[i][j];
+    }
+    
     bool isSubsequence(string s, string t) {
         if(s.length() == 0) return true;
-        return solve(s, t, 0, 0);
+        
+        // return solve(s, t, 0, 0);
+        
+        vector<vector<int>> dp(s.length()+1, vector<int>(t.length()+1, -1));
+        return solveMem(dp, s, t, 0, 0);
     }
 };

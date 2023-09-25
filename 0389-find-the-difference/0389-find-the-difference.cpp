fe3239cc5727ca4b62e48c994bbea2762e16f67f
Akash Gautam <76935019+geekblower@users.noreply.github.com>
2023-09-25 23:46:24 +0530

    Time: 6 ms (19.69%), Space: 7 MB (16.46%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -5,10 +5,9 @@ public:
         sort(t.begin(), t.end());
         
         for(int i=0; i<s.length(); i++) {
-            if(s[i] != t[i])
-                return t[i];
+            if(s[i] != t[i]) return t[i];
         }
         
-        return t[t.length()-1];
+        return t[t.length() - 1];
     }
 };

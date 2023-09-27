339861ac5933c388340951d134c653c022a38fc3
Akash Gautam <76935019+geekblower@users.noreply.github.com>
2023-09-27 23:03:48 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (13.97%) - LeetHub

---
+++ 1114-print-in-order/1114-print-in-order.cpp
@@ -0,0 +1,43 @@
+class Foo {
+private:
+    mutex m;
+    condition_variable cv;
+    int turn;
+    
+public:
+    Foo() {
+        turn = 0;
+    }
+
+    void first(function<void()> printFirst) {
+        
+        // printFirst() outputs "first". Do not change or remove this line.
+        printFirst();
+        
+        turn = 1;
+        cv.notify_all();
+    }
+
+    void second(function<void()> printSecond) {
+        unique_lock<mutex> lock(m);
+        while(turn != 1) {
+            cv.wait(lock);
+        }
+        
+        // printSecond() outputs "second". Do not change or remove this line.
+        printSecond();
+        
+        turn = 2;
+        cv.notify_all();
+    }
+
+    void third(function<void()> printThird) {
+        unique_lock<mutex> lock(m);
+        while(turn != 2) {
+            cv.wait(lock);
+        }
+        
+        // printThird() outputs "third". Do not change or remove this line.
+        printThird();
+    }
+};

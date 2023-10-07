38672f6f48862b400f6414a397bc910f809a753f
Akash Gautam <76935019+geekblower@users.noreply.github.com>
2023-10-07 23:50:26 +0530

    Time: 12 ms (31.18%), Space: 6.2 MB (30.38%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,14 +1,36 @@
+// class Solution {
+// public:
+//     bool isPalindrome(int x) {
+//         int temp = x;
+//         long long rev = 0;
+        
+//         while(temp) {
+//             rev = (rev*10) + (temp%10);
+//             temp /= 10;
+//         }
+        
+//         return (x==rev && x>=0) ? true : false;
+//     }
+// };
+
+
 class Solution {
 public:
     bool isPalindrome(int x) {
-        int temp = x;
-        long long rev = 0;
+        // int temp = x;
+        long long int palindrome = 0;
+        int num = x;
         
-        while(temp) {
-            rev = (rev*10) + (temp%10);
-            temp /= 10;
+        while(x != 0) {
+            long long int rem = x%10;
+            palindrome = palindrome*10 + rem;
+            x = x / 10;
         }
         
-        return (x==rev && x>=0) ? true : false;
+        if(palindrome == num && palindrome >= 0) {
+            return true;
+        } else {
+            return false;
+        }
     }
 };
